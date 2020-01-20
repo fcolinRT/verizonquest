01d22614378206082d796d70bbe4ee11a0ab26b6
Svogthirkev <metalslug_@hotmail.com>
2020-01-19 23:57:37 -0600

    Proyecto base
    Proyecto base de Unity con plugins de Oculus Quest y Lightwave Render Pipeline

---
+++ Verizon/Assets/Scripts/Socket/SocketManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class SocketManager : MonoBehaviour
+{
+
+    #region UNITY_METHODS
+    void Start()
+    {
+        
+    }
+    #endregion
+
+    #region PUBLIC_METHODS
+    #endregion
+
+    #region PRIVATE_METHODS
+    #endregion
+
+    #region COROUTINES
+    #endregion
+}
