260c875cf86b2018c38b64b69ca07bd29ec4f500
Rodolfo Quezada <raquezad.dev@gmail.com>
2020-01-27 18:27:30 -0800

    Branch creado para trabajar la funcionalidad de Sockets en Unity

---
+++ Verizon/Assets/Networking/SocketQuest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SocketIO;
+
+public class SocketQuest : SocketIOComponent
+{
+    public override void Start()
+    {
+        base.Start();
+    }
+
+    public override void Update()
+    {
+        base.Update();
+    }
+}
